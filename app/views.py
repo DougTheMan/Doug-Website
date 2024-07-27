3184becc4aace8d04415074abb97c7a3ea4b57ff
DougTheMan <mateuspansteingorges@gmail.com>
2024-07-27 10:15:23 -0300

    MORE

--- app/views.py
+++ app/views.py
@@ -2,9 +2,18 @@ from app import app
 from flask import render_template
 
 @app.route("/")
+#login required
 def index():
     return render_template('index.html')
 
-@app.route("/HomePage/")
+@app.route("/homepage/")
 def HomePage():
     return render_template('HomePage.html')
+
+@app.route("/portifolio/")
+def PortPage():
+    return render_template('PortPage.html')
+
+@app.route("/contacting/")
+def ContactPage():
+    return render_template('ContactPage.html')
